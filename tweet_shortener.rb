725b7528ef4cf83276a1ab7ac0fc93347c7f427c
jason brown <j.grant.brown@gmail.com>
2017-01-16 18:22:07 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,9 +25,9 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.size>140
-    puts word_substituter(tweet)
+  if tweet.length>140
+    word_substituter(tweet)
   else
-    puts tweet
+    tweet
   end
 end
