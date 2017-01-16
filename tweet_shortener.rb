705203bafc723aba55a1a890efe52e4d0275f3d6
jason brown <j.grant.brown@gmail.com>
2017-01-16 16:13:29 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def word_substituter(tweet)
+ dictionary={
+  "hello"=>"hi",
+  "to"=>"2",
+  "two"=>"2",
+  "too"=>"2",
+  "for"=>"4",
+  "four"=>"4",
+  "be"=>"b",
+  "you"=>"u",
+  "at"=>"@",
+  "and"=>"&"
+  }
+tweet_array=tweet.split(" ")
+ tweet_array.map {|word|
+    dictionary.each {|key,value|
+    word= value if word.downcase==key
+    }
+    word
+  }.join(" ")
+end
+end
