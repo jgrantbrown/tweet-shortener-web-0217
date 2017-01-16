1dbe17dcdfb54ec629432105edb2a85a5c8bb182
jason brown <j.grant.brown@gmail.com>
2017-01-16 18:28:04 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,7 +33,9 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  if selective_tweet_shortener(tweet).length=>140
-    return "#{word_substituter(tweet)[0..140]}..."
+  if selective_tweet_shortener(tweet).length>140
+    return "#{word_substituter(tweet)[0..136]}..."
+  else
+    return tweet
 end
 end
